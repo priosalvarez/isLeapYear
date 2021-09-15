1e34c72bdb1d5ac1cc08c92a7583e49a8163cadc
Pedro A. Rios <odautu@gmail.com>
2021-09-14 20:00:30 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear.isLeapYear(-1600));
+        System.out.println(isLeapYear.isLeapYear(1600));
+        System.out.println(isLeapYear.isLeapYear(2017));
+        System.out.println(isLeapYear.isLeapYear(2000));
+    }
+}
